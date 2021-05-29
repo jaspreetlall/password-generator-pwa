f4401a2a8f51741cedaa8ddc74bc788fcbb817c3
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-05-28 20:32:01 -0700

    Initialised folder structure and PWA setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
