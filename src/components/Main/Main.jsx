8c91ef649cfcba063a18fce3292e13c8c131dbb1
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-05-31 18:58:09 -0700

    Mobile styling for Main component complete

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import './Main.scss';
 
 function Main() {
 
-  const [ passwordLength, setPasswordLength ] = useState(20);
+  const [ passwordLength, setPasswordLength ] = useState(16);
   const [ includeLowercase, setIncludeLowercase ] = useState(true);
   const [ includeUppercase, setIncludeUppercase ] = useState(true);
   const [ includeNumbers, setIncludeNumbers ] = useState(true);
@@ -29,60 +30,86 @@ function Main() {
   const lowercaseCharCodes = arrayFromLowToHigh(97, 122);
   const uppercaseCharCodes = arrayFromLowToHigh(65, 90);
   const numberCharCodes = arrayFromLowToHigh(48, 57);
-  const symbolCharCodes = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126));
+  const symbolCharCodes = arrayFromLowToHigh(33, 47)
+    .concat(arrayFromLowToHigh(58, 64))
+    .concat(arrayFromLowToHigh(91, 96))
+    .concat(arrayFromLowToHigh(123, 126));
 
+  // Password generation logic
   const generatePassword = (event) => {
     event.preventDefault();
     let charCodesPool = [];
+    // Adding charCodes to the pool dependingon user choices
     if (includeLowercase) charCodesPool = charCodesPool.concat(lowercaseCharCodes);
     if (includeUppercase) charCodesPool = charCodesPool.concat(uppercaseCharCodes);
     if (includeNumbers) charCodesPool = charCodesPool.concat(numberCharCodes);
     if (includeSymbols) charCodesPool = charCodesPool.concat(symbolCharCodes);
 
     let passwordChars = [];
+    // Adding random characters to the passwordChars array
     for (let i = 0; i < passwordLength; i++) {
       let randomNumber = Math.floor(Math.random() * charCodesPool.length);
       let charCode = charCodesPool[randomNumber];
       passwordChars.push(String.fromCharCode(charCode));
     }
 
+    // Joining the passwordChars array as string to setGeneratedPassword
     setGeneratedPassword(passwordChars.join(''));
   }
 
   const isButtonDisabled = (!includeLowercase && !includeUppercase && !includeNumbers && !includeSymbols);
 
   return (
-    <main>
-      <h2>Include</h2>
-      <form onSubmit={generatePassword}>
-        <div>
-          <label htmlFor="passwordLength">Password Length: {passwordLength}</label>
-          <input type="range" name="passwordLength" id="passwordLength" min="8" max="24" value={passwordLength} onChange={handlePasswordLength}/>
+    <main className="main">
+      <form className="main__form container" onSubmit={generatePassword}>
+        <h2 className="main__form-heading">Generate Password</h2>
+        <div className="main__form-block main__form-block--password">
+          <h3 className="main__form-block-title">Generated Password</h3>
+          {(generatedPassword)
+          ? (
+            <div className="main__form-block-password">
+              <p className="main__form-block-password-text ">{generatedPassword}</p>
+            </div>
+          )
+          : (
+            <div className="main__form-block-password">
+              <p className="main__form-block-password-text main__form-block-password-text--none">Password will be displayed here</p>
+            </div>
+          )}
         </div>
-        <div>
-          <label htmlFor="includeLowercase">Lowercase</label>
-          <input type="checkbox" name="includeLowercase" id="includeLowercase" checked={includeLowercase} onChange={handleCheckboxLower}/>
+        <div className="main__form-block main__form-block--length">
+          <h3 className="main__form-block-title">Length: {passwordLength}</h3>
+          <div className="main__form-block-input">
+            <span className="main__form-block-input-span">8</span>
+            <input className="main__form-block-input-field main__form-block-input-field--length" type="range" name="passwordLength" id="passwordLength" min="8" max="24" value={passwordLength} onChange={handlePasswordLength}/>
+            <span className="main__form-block-input-span">24</span>
+          </div>
         </div>
-        <div>
-          <label htmlFor="includeUppercase">Uppercase</label>
-          <input type="checkbox" name="includeUppercase" id="includeUppercase" checked={includeUppercase} onChange={handleCheckboxUpper}/>
+
+        <div className="main__form-block main__form-block--settings">
+          <h3 className="main__form-block-title">Settings</h3>
+          <div className="main__form-block-input">
+            <label className="main__form-block-input-label" htmlFor="includeLowercase">Include lowercase</label>
+            <input className="main__form-block-input-field" type="checkbox" name="includeLowercase" id="includeLowercase" checked={includeLowercase} onChange={handleCheckboxLower}/>
+          </div>
+          <div className="main__form-block-input">
+            <label className="main__form-block-input-label" htmlFor="includeUppercase">Include uppercase</label>
+            <input className="main__form-block-input-field" type="checkbox" name="includeUppercase" id="includeUppercase" checked={includeUppercase} onChange={handleCheckboxUpper}/>
+          </div>
+          <div className="main__form-block-input">
+            <label className="main__form-block-input-label" htmlFor="includeNumbers">Include numbers</label>
+            <input className="main__form-block-input-field" type="checkbox" name="includeNumbers" id="includeNumbers" checked={includeNumbers} onChange={handleCheckboxNumbers}/>
+          </div>
+          <div className="main__form-block-input">
+            <label className="main__form-block-input-label" htmlFor="includeSymbols">Include symbols</label>
+            <input className="main__form-block-input-field" type="checkbox" name="includeSymbols" id="includeSymbols" checked={includeSymbols} onChange={handleCheckboxSymbols}/>
+          </div>
         </div>
-        <div>
-          <label htmlFor="includeNumbers">Numbers</label>
-          <input type="checkbox" name="includeNumbers" id="includeNumbers" checked={includeNumbers} onChange={handleCheckboxNumbers}/>
-        </div>
-        <div>
-          <label htmlFor="includeSymbols">Symbols</label>
-          <input type="checkbox" name="includeSymbols" id="includeSymbols" checked={includeSymbols} onChange={handleCheckboxSymbols}/>
-        </div>
-        <div>
-          <span>{ generatedPassword ? generatedPassword : "Password will be displayed here" }
-          </span>
-        </div>
-        <div>
-          <button type="submit" disabled={isButtonDisabled}>Generate Password</button>
-          <button type="reset">Reset</button>
-        </div>
+
+        <div className="main__form-block main__form-block--buttons">
+          <button className="main__form-block-button main__form-block-button--submit" type="submit" disabled={isButtonDisabled}>Generate Password</button>
+          <button className="main__form-block-button" type="button">Copy</button>
+        </div>        
       </form>
     </main>
   )
