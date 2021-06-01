5d5bd7b038b2eede8c5adfe1f4dee9f3732e9f35
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-06-01 12:46:49 -0700

    Added app logo credits

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,6 +8,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer__block container">
+        <a className="footer__block-link footer__block-link--credit" href="https://freeicons.io/profile/823" target="_blank" rel="noreferrer noopener">App Icon by Muhammad Haq</a>
         <a className="footer__block-link" href="https://github.com/jaspreetlall/password-generator-pwa" target="_blank" rel="noreferrer noopener">
           <img className="footer__block-link-logo" src={GitHubLogo} alt="Github"/>
         </a>
