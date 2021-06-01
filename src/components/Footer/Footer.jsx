040c441ccb6625ea7d0ff37b2e0a914e4ee16060
Jaspreet Singh Lall <jaspreet.lall@gmail.com>
2021-05-31 22:53:49 -0700

    Added and styled footer component for mobile layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './Footer.scss';
+import GitHubLogo from '../../assets/logo/contact-github.png';
+import LinkedInLogo from '../../assets/logo/contact-linkedin.png';
+
 
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
+    <footer className="footer">
+      <div className="footer__block container">
+        <a className="footer__block-link" href="https://github.com/jaspreetlall/password-generator-pwa" target="_blank" rel="noreferrer noopener">
+          <img className="footer__block-link-logo" src={GitHubLogo} alt="Github"/>
+        </a>
+        <a className="footer__block-link" href="https://linkedin.com/in/jaspreetlall" target="_blank" rel="noreferrer noopener">
+          <img className="footer__block-link-logo" src={LinkedInLogo} alt="LinkedIn" />
+        </a>
+      </div>
+    </footer>
   )
 }
 
